ef8b0bf25cc74b378d11cbdb2200a818b3c04361
Kyra Jaeger <jaeger.kyrae@gmail.com>
2021-10-13 12:58:59 -0500

    Completed input/variables. Verified that values match expected numbers.

--- Daily Coding Problems/Problem_001/Problem1.cpp
+++ Daily Coding Problems/Problem_001/Problem1.cpp
@@ -7,16 +7,21 @@ int main() {
 
 	int numList[5];
 
-	for (int i = 0; i < 10; i++) {
+	cout << "This program takes a list of numbers to see if any two equal the sum of another specificied number (k)." << endl;
+	cout << "Let's enter the list of numbers first. Chose 5 numbers." << endl;
 
+	for (int i = 0; i < 5; i++) {
+
+		cout << "Enter number " << i + 1 << ": " << endl;
+		cin >> numList[i];
 	}
 
-	cout << "Give 5 numbers" << endl;
-	
-
 	int k;
-	cout << "Give a number (k)" << endl;
+	cout << "What is the specified sum (k)?" << endl;
 	cin >> k;
+
+	cout << "The list includes: " << numList[0] << ", " << numList[1] << ", " << numList[2] << ", " << numList[3] << ", " << numList[4] << ". " << endl;
+	cout << "Number k is: " << k << endl;
 
 	return 0;
 
