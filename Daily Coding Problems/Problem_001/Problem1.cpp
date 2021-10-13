8650cd24eb63b00017400dd90f0d48070f6f26a3
Kyra Jaeger <jaeger.kyrae@gmail.com>
2021-10-13 12:46:03 -0500

    initialized an array and int k

--- Daily Coding Problems/Problem_001/Problem1.cpp
+++ Daily Coding Problems/Problem_001/Problem1.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include "Problem1.h"
 using namespace std;
 
 
 int main() {
-	cout << "testing code...";
 
+	int numList[5];
+
+	for (int i = 0; i < 10; i++) {
+
+	}
+
+	cout << "Give 5 numbers" << endl;
+	
+
+	int k;
+	cout << "Give a number (k)" << endl;
+	cin >> k;
 
 	return 0;
 
