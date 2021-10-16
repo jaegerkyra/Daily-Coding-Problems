7fc18ea25f07c77dcdfa0c332a7c05e88436488b
Kyra Jaeger <jaeger.kyrae@gmail.com>
2021-10-15 21:59:58 -0500

    came up with an outline and started skeleton for program

---
+++ Daily Coding Problems/Problem_002/Problem2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int numList[5];
+
+	cout << "Enter 5 numbers" << endl;
+
+	for (int i = 0; i < 5; i++) {
+
+		cout << "Enter number " << i + 1 << ": " << endl;
+		cin >> numList[i];
+	}
+
+	for (int i = 0; i < 5; i++) {
+		//Need to come up with a formula or solution to multiply and reassign to array
+	}
+
+	return 0;
+
+}
