8545f854c040419c9d60b71cceb48ce78a44b197
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2021-10-27 16:44:16 -0500

    Create Problem_005.cpp
    Started pseudocode for problem

---
+++ Daily Coding Problems/Problem_005/Problem_005.cpp
@@ -0,0 +1,30 @@
+/*
+//Program to convert Roman numerals to integers
+Symbol       Value
+        I             1
+        V             5
+        X             10
+        L             50
+        C             100
+        D             500
+        M             1000
+*/
+
+#include <iostream>
+
+int main()
+{
+    //If char[x] == I, then char[x] = 1
+    //If char[x] == V, then char[x] = 5
+    //If char[x] == X, then char[x] = 10
+    //If char[x] == L, then char[x] = 50
+    //If char[x] == C, then char[x] = 100
+    //If char[x] == D, then char[x] = 500
+    //If char[x] == M, then char[x] = 1000
+
+    //If char to the left is less than one on the right, subtract. Otherwise, numbers should be added
+    
+
+    return 0;
+
+}
